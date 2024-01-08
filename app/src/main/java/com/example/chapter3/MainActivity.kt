ff5a71f8a0ee689fcef6439941d6b7cfcab790b0
suyeong2785 <qkdtpwls_@naver.com>
2024-01-08 15:33:32 +0900

    24/01/08 방수영 1.사용자화면을 통해서 로컬ip주소를 입력해서 받아온 response body값을 화면에 보여주도록 기능 구현

--- app/src/main/java/com/example/chapter3/MainActivity.kt
+++ app/src/main/java/com/example/chapter3/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.chapter3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
@@ -17,29 +24,57 @@ import java.net.ServerSocket
 import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val editText = findViewById<EditText>(R.id.serverHostEditText)
+        val confirmButton = findViewById<Button>(R.id.confirmButton)
+        val informationTextView = findViewById<TextView>(R.id.informationTextView)
         val client = OkHttpClient()
+        var serverHost = ""
 
-        val request: Request = Request.Builder()
-            .url("http://192.168.200.123:8080")
-            .build()
-
-        val callback = object: Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.e("Client", e.toString())
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                if(response.isSuccessful){
-                    Log.e("Client", "${response.body?.string()}")
-                }
-            }
+        editText.addTextChangedListener {
+            serverHost = it.toString()
         }
 
-        val response = client.newCall(request).enqueue(callback)
+        confirmButton.setOnClickListener {
+            val request: Request = Request.Builder()
+                .url("http://$serverHost:8080")
+                .build()
+
+            val callback = object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "수신에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    Log.e("Client", e.toString())
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        val response = response.body?.string()
+
+                        runOnUiThread {
+                            informationTextView.isVisible = true
+                            informationTextView.text = response
+
+                            editText.isVisible = false
+                            confirmButton.isVisible = false
+                        }
+                    } else {
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, "수신에 실패했습니다.", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                }
+            }
+
+            val response = client.newCall(request).enqueue(callback)
+        }
+
 
         /*Thread {
             try {
